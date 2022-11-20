fa509bcf1b63f09814aeb8bc86e2d03f7525ac95
antha <ahajriansah@gmail.com>
2022-11-20 20:08:37 +0800

    MyListView

---
+++ app/src/main/java/com/f55121046_hajriansah/mylistview/anta.java
@@ -0,0 +1,18 @@
+package com.f55121046_hajriansah.mylistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ListView;
+
+public class anta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anta);
+    }
+}
